d31b67cd7661c19b796a63e0f3c8ab33381d6efa
FediSalhi <fadi.salhi@outlook.fr>
2020-11-24 23:23:16 +0300

    receiver code updated

--- Receiver/receiver/receiver.ino
+++ Receiver/receiver/receiver.ino
@@ -3,11 +3,11 @@
 #include <RF24.h>
 RF24 radio(9, 10); // CE, CSN
 const byte address[6] = "00001";
-boolean button_state = 0;
-int led_pin = 3;
+
 void setup() {
-pinMode(6, OUTPUT);
+
 Serial.begin(9600);
+
 radio.begin();
 radio.openReadingPipe(0, address);   //Setting the address at which we will receive the data
 radio.setPALevel(RF24_PA_MIN);       //You can set this as minimum or maximum depending on the distance between the transmitter and receiver.
@@ -15,20 +15,14 @@ radio.startListening();              //This sets the module as receiver
 }
 void loop()
 {
-if (radio.available())              //Looking for the data.
+if (radio.available())                             //Looking for the data.
 {
-char text[32] = "";                 //Saving the incoming data
-radio.read(&text, sizeof(text));    //Reading the data
-radio.read(&button_state, sizeof(button_state));    //Reading the data
-if(button_state == HIGH)
-{
-digitalWrite(6, HIGH);
-Serial.println(text);
+char voltage_str[32] = "";                        //Saving the incoming data
+radio.read(&voltage_str, sizeof(voltage_str));    //Reading the data
+
+Serial.println(voltage_str);                      // aryuze bas
+
 }
-else
-{
-digitalWrite(6, LOW);
-Serial.println(text);}
-}
+
 delay(5);
 }
